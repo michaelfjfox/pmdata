d77507720aefdc9678c00281710f15d5d965cea6
Michael Fox <michaelfjfox@gmail.com>
2019-07-14 16:10:01 +0100

    Daily crontab update Sun 14 Jul 16:10:01 BST 2019

--- commits.sh
+++ commits.sh
@@ -2,4 +2,4 @@
 cd /home/pi/pmdata && git add --all && git commit -m "Daily crontab update `date`"
 
 # Push to git server
-cd /home/pi/pmdata && git push origin master
+cd /home/pi/pmdata && git push 
